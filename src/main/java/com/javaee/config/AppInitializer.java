cbdb1e46077bacfbc1a2657688585da9091196f3
arafin <sami.cse.1112@gmail.com>
2025-03-08 23:45:17 +0900

    converted xml based config to java based config

---
+++ src/main/java/com/javaee/config/AppInitializer.java
@@ -0,0 +1,16 @@
+package com.javaee.config;
+
+import jakarta.servlet.ServletContext;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+public class AppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(SpringConfig.class);
+        servletContext.addListener(new ContextLoaderListener(context));
+    }
+}
